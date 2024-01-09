ed97307f0d2b23fffaa013cb3f9d96021a250f26
Tom Fahy <thomas.fahy9@gmail.com>
2024-01-09 19:37:14 +0000

    add array of question objects for inquirer

--- index.js
+++ index.js
@@ -5,7 +5,46 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'project',
+      },
+      {
+        type: 'input',
+        message: 'Enter a short description of your project: ',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'Describe how to use your application:',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Describe how other developers should contribute to the project:',
+        name: 'contribute',
+      },
+      {
+        type: 'input',
+        message: 'Describe how to test your project:',
+        name: 'test',
+      },
+      {
+        type: 'input', //find correct type from inquirer logic
+        message: 'Choose a license for your project:',
+        name: 'license',
+      },
+      {
+        type: 'input',
+        message: 'Please add a link to your github profile:',
+        name: 'license',
+      },
+      {
+        type: 'input', 
+        message: 'Please add your email:',
+        name: 'license',
+      },
 ];
 
 // function to write README file
